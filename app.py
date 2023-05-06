b45f436839440f2dd956c28da79c9c3375b0382c
Mayara <93294435+mayarakaren@users.noreply.github.com>
2023-05-05 22:34:35 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -41,12 +41,45 @@ def excluir():
     else:
         conectar = mysql.connect(host="localhost", user="root", password="", database="exemplo")
         cursor = conectar.cursor()
-        cursor.execute("DELETE FROM teste WHERE codigo='"+ txt_codigo.get() +"'")
+        cursor.execute("DELETE FROM teste WHERE cod='"+ txt_codigo.get() +"'")
         cursor.execute("commit")
         MessageBox.showinfo("Mensagem", "Informação Excluída com Sucesso!")
         conectar.close()
 
+def atualizar():
+    id = txt_codigo.get()
+    name = txt_nome.get()
+    phone = txt_telefone.get()
+
+    if(name == "" or phone == ""):
+        MessageBox.shoinfo("ALERT", "Digite todos os campos para realizar alteração")
+    else:
+        conectar = mysql.connect(host="localhost", user="root", password="", database="exemplo")
+        cursor = conectar.cursor()
+        cursor.execute("UPDATE teste SET nome = '"+ txt_nome.get() + "', telefone = '" + txt_telefone.get() + "' WHERE cod='"+ txt_codigo.get() + "'")
+        cursor.execute("commit")
+
+    MessageBox.showinfo("Status", "Atualização feita com sucessão!")
+    conectar.close()
+
+def Select():
+    if(txt_codigo.get() == ""):
+        MessageBox.showinfo("ALERT", "Por favor Digite o Código")
+    else:
+        conectar = mysql.connect(host="localhost", user="root", password="", database="exemplo")
+        cursor = conectar.cursor()
+        cursor.execute("SELECT * FROM teste WHERE cod= '"+ txt_codigo.get() + "'")
+        rows = cursor.fetchall()
+
+        for row in rows:
+            txt_nome.insert(0, row[1])
+            txt_telefone.insert(0, row[2])
+
+        conectar.close()
+
 btn_cadastrar = Button(interface, text="Salvar", command=salvar, font=("Arial 12")).place(x=100, y=190)
 btn_excluir = Button(interface, text="Apagar", command=excluir, font=("Arial 12")).place(x=200, y=190)
+btn_update = Button(interface, text="Atualizar", command=atualizar, font=("Arial 12")).place(x=320, y=190)
+btn_consultar= Button(interface, text="Consultar", command=Select, font=("Arial 12")).place(x=200, y=240)
 
 interface.mainloop()
